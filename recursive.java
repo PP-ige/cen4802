808b13babbf5a9f8d030972de5af0f83bcfd59df
PP-ige <134177148+PP-ige@users.noreply.github.com>
2024-01-20 18:40:55 -0500

    Create recursive.java

---
+++ recursive.java
@@ -0,0 +1,15 @@
+public class recursive{
+  public static int fib(int n){
+    if( n<=1)
+      return n;
+    
+    return fib(n-1) + fib(n-2);
+  }
+  
+  public static void main(String args[]){
+      int n =10;
+      int y = fib(n);
+      
+      System.out.println("The "+n+"th term of the Fibonacci sequence is "+y);
+  }//end of main
+}//end of recursive
